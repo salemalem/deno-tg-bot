09283e6e403266194a39ee37cab07d79f7af959d
Shyngys Shynbolatov <shyngys.shynbolatov@gmail.com>
2022-01-10 09:26:37 +0700

    comments to server added

--- server.js
+++ server.js
@@ -3,6 +3,10 @@ import Denoot from "https://deno.land/x/denoot/mod.ts";
 
 const app = Denoot.app(3000);
 
+console.log("Server is running..")
 app.get("/", (req, res) => {
     res.send("Hello World!");
 });
+
+// then run "deno run --allow-net --unstable server.ts"
+// open http://localhost:3000
